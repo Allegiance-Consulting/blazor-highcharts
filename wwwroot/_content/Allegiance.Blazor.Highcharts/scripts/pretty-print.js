6d111ae74f9d7c1fb5ac44ab48fd78b5a1ab09fb
Allegiance-Consulting <60039385+Allegiance-Consulting@users.noreply.github.com>
2020-04-14 23:44:37 +0000

    Deploying to gh-pages from  @ 56760d7f440b15272c172f7f923fc668111ce1fb 🚀

---
+++ wwwroot/_content/Allegiance.Blazor.Highcharts/scripts/pretty-print.js
@@ -0,0 +1,9 @@
+﻿window.prettyPrint = function () {
+    setTimeout(function () {
+        PR.prettyPrint();
+        var cols = document.getElementsByClassName('opacity-load');
+        for (i = 0; i < cols.length; i++) {
+            cols[i].classList.remove('opacity-load');
+        }
+    }, 700);
+}

c157c0d7c369f5b50e78bc658fdfd0d085faa108
RonnieNiezenAllegiance <RonnieNiezenAllegiance@users.noreply.github.com>
2020-06-05 10:35:16 +0000

    Deploying to gh-pages from  @ 2912de4ab372524f29795f84d40b9f6a09cf148f 🚀

--- _content/Allegiance.Blazor.Highcharts/scripts/blazor-highcharts.js
+++ _content/Allegiance.Blazor.Highcharts/scripts/blazor-highcharts.js
@@ -1,7 +1,7 @@
 ﻿// This is the interop Javascript file that handles all interop between Blazor and Javascript
 
 window.renderHighchartChart = function (containerId, chartOptions) {
-    debugger
+    //debugger
     let options = '';
     try {
         options = JSON.parse(chartOptions, this.reviver);

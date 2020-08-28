8d60a3471efbe1c934b7e08efc1b173d0fc45dc7
Ronnie Niezen <ronnie.niezen@allegiance.co.za>
2020-08-28 08:07:54 +0200

    added collumnRange Chart type and chartoptions

---
+++ src/Allegiance.Blazor.Highcharts/Options/PlotOptionsColumnRange.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+
+namespace Allegiance.Blazor.Highcharts.Options
+{
+    public partial class PlotOptionsColumnRange
+    {
+        [JsonProperty("borderRadius", NullValueHandling = NullValueHandling.Ignore)]
+        public int? BorderRadius { get; set; }
+
+        public PlotOptionsColumnRange()
+        {
+
+        }
+
+
+
+        public PlotOptionsColumnRange(int? borderRadius)
+        {
+            BorderRadius = borderRadius;
+        }
+    }
+}
